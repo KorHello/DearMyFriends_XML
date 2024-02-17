9ed01aa0f524ee44e3723596348affc3c127e6c9
김준수 <ypd603@naver.com>
2024-02-17 13:21:19 +0900

    * shared 매니저 완성

--- app/src/main/java/com/devhouse/dearmyfriends/MainActivity.kt
+++ app/src/main/java/com/devhouse/dearmyfriends/MainActivity.kt
@@ -5,6 +5,8 @@ import com.devhouse.dearmyfriends.base.BaseActivity
 class MainActivity: BaseActivity(R.layout.activity_main){
     override fun initModel() {
         super.initModel()
+
+//        this.prefer
     }
 
     override fun initAction() {
--- app/src/main/java/com/devhouse/dearmyfriends/mng/SharedManager.kt
+++ app/src/main/java/com/devhouse/dearmyfriends/mng/SharedManager.kt
@@ -1,11 +1,34 @@
 package com.devhouse.dearmyfriends.mng
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.devhouse.dearmyfriends.base.BaseActivity
+
 class SharedManager {
-    companion object {
-        val instance = SharedManager()
+    private var activity: BaseActivity
+    private var pref: SharedPreferences
+
+    constructor(activity: BaseActivity) {
+        this.activity = activity
+        this.pref = this.activity.getPreferences(Context.MODE_PRIVATE)
     }
 
+    /* about preference */
+    fun setString(key: SharedKey, value: String) {
+        val editor = pref.edit()
+        editor.putString(key.key, value)
+        editor.apply()
+    }
 
+    fun getString(key: SharedKey): String {
+        var def = ""
+
+        if(pref.getString(key.key, "") != null) {
+            def = pref.getString(key.key, "")!!
+        }
+
+        return def
+    }
 }
 
 enum class SharedKey(var key: String) {
